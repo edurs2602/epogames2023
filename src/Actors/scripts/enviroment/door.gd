0323911f9b17c83497657c7af72218fddb4f92f1
marioxaxa <marioxaxa0@gmail.com>
2023-09-23 20:21:00 -0300

    create branch

---
+++ src/Actors/scripts/enviroment/door.gd
@@ -0,0 +1,29 @@
+extends StaticBody2D
+
+@export var is_open = false
+@export var door_group = "none"
+@export var number_of_levers = 0
+var atived_levers = 0
+
+func door():
+	pass
+	
+func open_door():
+	is_open = true
+	self.set_collision_layer_value(3,false)
+	
+func close_door():
+	is_open = false
+	self.set_collision_layer_value(3,true)
+
+
+func _on_torch_torch_lighted(torch_group):
+	
+	print(torch_group)
+	
+	if torch_group == door_group:
+		atived_levers += 1
+	
+	if atived_levers == number_of_levers:
+		open_door()
+
