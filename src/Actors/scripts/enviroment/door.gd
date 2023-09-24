9054c542bdc61e55b8fe5e53119424b73888cfdd
marioxaxa <marioxaxa0@gmail.com>
2023-09-24 16:53:06 -0300

    power  menu e doors

--- src/Actors/scripts/enviroment/door.gd
+++ src/Actors/scripts/enviroment/door.gd
@@ -5,11 +5,17 @@ extends StaticBody2D
 @export var number_of_levers = 0
 var atived_levers = 0
 
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _ready():
+	animated_sprite.play("door_closed")
+
 func door():
 	pass
 	
 func open_door():
 	is_open = true
+	animated_sprite.play("door_opened")
 	self.set_collision_layer_value(3,false)
 	
 func close_door():
@@ -27,3 +33,11 @@ func _on_torch_torch_lighted(torch_group):
 	if atived_levers == number_of_levers:
 		open_door()
 
+
+
+func _on_torch_2_torch_lighted(torch_group):
+	if torch_group == door_group:
+		atived_levers += 1
+	
+	if atived_levers == number_of_levers:
+		open_door()
