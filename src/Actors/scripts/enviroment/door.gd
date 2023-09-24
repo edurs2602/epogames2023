30895525f069164749fd5fab0e7aa8ad945a44b9
edurs2602 <edurs.2602@gmail.com>
2023-09-24 09:25:42 -0300

    feat: animação da porta

--- src/Actors/scripts/enviroment/door.gd
+++ src/Actors/scripts/enviroment/door.gd
@@ -11,10 +11,13 @@ func door():
 func open_door():
 	is_open = true
 	self.set_collision_layer_value(3,false)
+	$AnimatedSprite2D.play("door_opening")
+	$AnimatedSprite2D.play("door_opened")
 	
 func close_door():
 	is_open = false
 	self.set_collision_layer_value(3,true)
+	$AnimatedSprite2D.play("door_closed")
 
 
 func _on_torch_torch_lighted(torch_group):
